25b12f6d91c84b5322bf76d68b43967ee6d74454
SongshGeo <songshgeo@gmail.com>
2023-09-28 21:29:04 -0500

    test(agents): :test_tube: farmer attributes and diffuse

--- src/farmer.py
+++ src/farmer.py
@@ -5,17 +5,53 @@
 # GitHub   : https://github.com/SongshGeo
 # Website: https://cv.songshgeo.com/
 
+import os
 from typing import Self
 
 import numpy as np
+from hydra import compose, initialize
 
 from src.people import SiteGroup
+
+# 加载项目层面的配置
+with initialize(version_base=None, config_path="../config"):
+    cfg = compose(config_name="config")
+os.chdir(cfg.root)
+
+INIT_AREA = cfg.farmer.area
 
 
 class Farmer(SiteGroup):
     """
     农民类
     """
+
+    def __init__(self, *arg, **kwargs) -> None:
+        self._area = INIT_AREA
+        super().__init__(*arg, **kwargs)
+        self._growth_rate = self.params.growth_rate
+
+    @property
+    def growth_rate(self) -> float:
+        """人口增长率，可以因复杂化而下降"""
+        return self._growth_rate
+
+    @growth_rate.setter
+    def growth_rate(self, growth_rate) -> None:
+        """人口增长率变化"""
+        growth_rate = max(growth_rate, 0)
+        self._growth_rate = float(growth_rate)
+
+    @property
+    def area(self) -> float:
+        """耕地面积"""
+        return self._area
+
+    @area.setter
+    def area(self, area: float) -> None:
+        """耕地面积变化，会不断增加"""
+        area = max(self.area, area)
+        self._area = float(area)
 
     @property
     def size(self) -> int:
@@ -25,6 +61,7 @@ class Farmer(SiteGroup):
     def size(self, size):
         """人口规模有最大最小值限制"""
         if size < self.params.min_size:
+            # * 这里是不是少于就死了
             self.die()
         elif size > self.max_size:
             self.complicate()
@@ -33,20 +70,20 @@ class Farmer(SiteGroup):
     @property
     def max_size(self) -> float:
         """最大人口数量"""
-        return self.params.area * np.pi**2 * 2 / 0.005
+        # 参考对裴李岗时期（9000-7000 BP）人均所需耕地为0.008平方公里的数据（乔玉 2010），结合华南气候条件下较高的生产力和更充沛的自然资源，将所需人均耕地设置为0.004平方公里，那么该单位人口上限即π2*2/0.004=3142人
+        return self.area * np.pi**2 * 2 / 0.004
 
     def diffuse(self, force: bool = False) -> Self:
         """农民的分散"""
         if force:
             return super().diffuse()
         cond1 = self.size >= self.loc("lim_h")
-        cond2 = np.random.random() < self.params.diffuse_prob
+        cond2 = self.random.random() < self.params.diffuse_prob
         if cond1 and cond2:
-            super().diffuse()
+            return super().diffuse()
 
     def complicate(self) -> Self:
         """农民的复杂化"""
         # 耕地上限再增加耕地密度增加、人口增长率下降
-        self.max_size *= self.params.complexity
-        # sub_group = None
-        # return sub_group
+        self.growth_rate *= 1 - self.params.complexity
+        self.area += INIT_AREA * (1 - self.params.complexity)
---
+++ tests/test_farmer.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python 3.11.0
+# -*-coding:utf-8 -*-
+# @Author  : Shuang (Twist) Song
+# @Contact   : SongshGeo@gmail.com
+# GitHub   : https://github.com/SongshGeo
+# Website: https://cv.songshgeo.com/
+
+import os
+from unittest.mock import MagicMock
+
+import pytest
+from hydra import compose, initialize
+
+from abses import MainModel
+from src.env import CompetingCell
+from src.farmer import Farmer
+from src.hunter import Hunter
+
+# 加载项目层面的配置
+with initialize(version_base=None, config_path="../config"):
+    cfg = compose(config_name="config")
+os.chdir(cfg.root)
+
+MIN_SIZE = cfg.farmer.min_size
+G_RATE = cfg.farmer.growth_rate
+AREA = cfg.farmer.area
+
+
+class TestFarmer:
+    @pytest.fixture(name="raw_model")
+    def mock_model(self) -> MainModel:
+        """一个虚假的模型"""
+        model = MainModel(parameters=cfg)
+        layer = model.nature.create_module(
+            how="from_resolution",
+            shape=(4, 4),
+            cell_cls=CompetingCell,
+        )
+        farmer = model.agents.create(Farmer, singleton=True)
+        cell = layer.array_cells[3][2]
+        # 模拟最大可支持的人口规模
+        cell.lim_h = cfg.hunter.settle_size
+        # 将虚假的农民放到它旁边
+        farmer.put_on(layer.array_cells[3][2])
+        hunter = model.agents.create(Hunter, size=50, singleton=True)
+        return model, hunter, farmer, cell
+
+    @pytest.fixture(name="farmer")
+    def mock_farmer(self, raw_model) -> Farmer:
+        """一个虚假的农民"""
+        _, _, farmer, _ = raw_model
+        return farmer
+
+    @pytest.fixture(name="cell")
+    def mock_cell(self, raw_model) -> CompetingCell:
+        """一个虚假的格子"""
+        _, _, _, cell = raw_model
+        return cell
+
+    def test_init(self, farmer):
+        """测试初始化"""
+        # Arrange / Act / Assert
+        assert farmer.growth_rate == G_RATE
+        assert farmer.area == AREA
+
+    @pytest.mark.parametrize(
+        "growth_rate, expected",
+        [
+            (0.1, 0.1),
+            (0.2, 0.2),
+            (-0.1, 0),
+            (-0.2, 0),
+        ],
+        ids=[
+            "positive_growth_rate",
+            "positive_growth_rate",
+            "negative_growth_rate",
+            "negative_growth_rate",
+        ],
+    )
+    def test_growth_rate_setter(self, farmer, growth_rate, expected):
+        """测试人口增长率变化"""
+        # Arrange / Act
+        farmer.growth_rate = growth_rate
+        result = farmer.growth_rate
+
+        # Assert
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        "area, expected",
+        [
+            (1, 2),
+            (2, 2),
+            (3, 3),
+            (-100, 2),
+        ],
+        ids=["positive_area", "positive_area", "zero_area", "negative_area"],
+    )
+    def test_area_setter(self, farmer, area, expected):
+        """测试耕地面积变化"""
+        # Arrange / Act
+        assert farmer.area == AREA
+        farmer.area = area
+
+        # Assert
+        assert farmer.area == expected
+
+    @pytest.mark.parametrize(
+        "force, size, lim_h, diffuse_prob, expected_result",
+        [
+            (True, 100, 50, 0.0, True),
+            (False, 10, 50, 0.1, False),
+            (False, 100, 50, 0.1, False),
+            (False, 10, 50, 0.04, False),
+            (False, 100, 50, 0.04, True),
+        ],
+        ids=[
+            "force_true",
+            "cond1_false_cond2_false",
+            "cond1_true_cond2_false",
+            "cond1_false_cond2_true",
+            "cond1_true_cond2_true",
+        ],
+    )
+    def test_diffuse(
+        self, farmer, cell, force, size, lim_h, diffuse_prob, expected_result
+    ):
+        """测试农民的分散"""
+        # Arrange
+        farmer.size = size
+        cell.lim_h = lim_h
+        farmer.random.random = MagicMock(return_value=diffuse_prob)
+        # Act
+        result = farmer.diffuse(force)
+
+        # Assert
+        assert isinstance(result, Farmer) is expected_result
