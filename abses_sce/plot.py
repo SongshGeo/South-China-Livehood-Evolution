c823b6974ea097befc6378b74cfca0c277e715ba
SongshGeo <songshgeo@gmail.com>
2024-01-05 21:22:16 +0800

    feat(analysis): :sparkles: 增加水稻农民的绘图

--- abses_sce/plot.py
+++ abses_sce/plot.py
@@ -33,11 +33,13 @@ class ModelViz:
 
     def histplot(self) -> Axes:
         """绘制主体人数的分布直方图"""
-        _, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 3))
+        _, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(10, 3))
         sns.histplot(self.model.farmers.array("size"), ax=ax1)
         sns.histplot(self.model.hunters.array("size"), ax=ax2)
+        sns.histplot(self.model.rice.array("size"), ax=ax2)
         ax1.set_xlabel("Farmers")
         ax2.set_xlabel("Hunters")
+        ax3.set_xlabel("Rice Farmers")
         if self.save:
             plt.savefig(self.save / f"repeat_{self.repeats}_histplot.jpg")
             plt.close()
@@ -48,12 +50,14 @@ class ModelViz:
         _, ax = plt.subplots()
         ax.plot(self.data["farmers_num"], label="farmers size")
         ax.plot(self.data["hunters_num"], label="hunters size")
+        ax.plot(self.data["rice_num"], label="rice farmers")
         ax.set_xlabel("time")
         ax.set_ylabel("population")
         ax.legend()
         ax2 = ax.twinx()
         ax2.plot(self.data["len_farmers"], ls=":", label="farmers groups")
         ax2.plot(self.data["len_hunters"], ls=":", label="hunters groups")
+        ax2.plot(self.data["len_rice"], ls=":", label="rice farmers")
         if self.save:
             plt.savefig(self.save / f"repeat_{self.repeats}_dynamic.jpg")
             plt.close()
