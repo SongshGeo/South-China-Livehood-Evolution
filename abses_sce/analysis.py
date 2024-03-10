a35f4fcf67d82cba362b42e730ac737fdc4f6a48
SongshGeo <songshgeo@gmail.com>
2024-03-10 22:40:47 +0800

    feat(analysis): :sparkles: 现在可以检测人数哪一年发生了变化

--- abses_sce/analysis.py
+++ abses_sce/analysis.py
@@ -12,9 +12,52 @@ from typing import Callable, Iterable, List
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import ruptures as rpt
 import seaborn as sns
 import yaml
 from omegaconf import DictConfig
+
+
+def detect_breakpoints(
+    series: pd.Series, n_bkps: int = 1, algorithm="Dynp", min_size: int = 5
+) -> List[int] | int:
+    """检查序列数据的变化趋势间断点。
+
+    Parameters:
+        series:
+            需要判断的时间序列数据，索引是时间，值是判断趋势的值。
+        n_bkps:
+            预期有多少个间断点，默认为1.
+        algorithm:
+            依赖于包[ruptures](https://centre-borelli.github.io/ruptures-docs/)进行实现，从以下四种算法中选择一种:
+            - "[Dynp](https://centre-borelli.github.io/ruptures-docs/code-reference/detection/dynp-reference/)"
+            - "[Binseg](https://centre-borelli.github.io/ruptures-docs/code-reference/detection/binseg-reference/)"
+            - "[BottomUp](https://centre-borelli.github.io/ruptures-docs/code-reference/detection/bottomup-reference/)"
+            - "[Window](https://centre-borelli.github.io/ruptures-docs/code-reference/detection/window-reference/)"
+            默认选用 Dynp。
+        min_size:
+            切割时间序列后，每一段最小不能少于几个时间单位，默认为5。
+
+    Raises:
+        ValueError:
+            如果输入了不正确的算法名称。
+
+    Returns:
+        识别的断点所在的时间。
+    """
+    valid_algorithms = ["Dynp", "Binseg", "BottomUp", "Window"]
+    if algorithm not in valid_algorithms:
+        raise ValueError(
+            f"Algorithm should be chosen from {valid_algorithms}, got {algorithm} instead."
+        )
+    algorithm = getattr(rpt, algorithm, None)
+    algo = algorithm(model="l2", min_size=min_size)
+    algo.fit(series.values)
+    result = algo.predict(n_bkps=n_bkps)
+    breakpoints = [series.index[i] for i in result[:-1]]
+    if n_bkps == 1:
+        return breakpoints[0] if breakpoints else None
+    return breakpoints
 
 
 # 自定义堆积折线图绘制函数
@@ -219,6 +262,22 @@ class MultipleAnalysis:
         )
         return g
 
+    def detect_farmers_num_change_years(self, col: str = "farmers_num") -> pd.DataFrame:
+        """Detect the change time of farmers' number in each year."""
+        overrides = self.overrides.columns.to_list()
+        data = self.get_dataset()
+        grouped = data.groupby([*overrides, "time"])[col].mean().reset_index(level=-1)
+
+        change_time = []
+        for x, y in self.overrides.itertuples(index=False):
+            tmp_d = grouped.loc[(x, y)]
+            ser = pd.Series(tmp_d[col].values, index=tmp_d["time"].values)
+            change_time.append(detect_breakpoints(series=ser))
+
+        result = self.overrides.copy()
+        result["change_time"] = change_time
+        return result
+
 
 def main(path: str) -> None:
     """绘制路径"""
@@ -232,6 +291,8 @@ def main(path: str) -> None:
     plt.savefig(analysis.home / "analysis_num.jpg")
     analysis.grid_stackplots("len", *overriding_lst)
     plt.savefig(analysis.home / "analysis_len.jpg")
+    detected_change_years = analysis.detect_farmers_num_change_years()
+    detected_change_years.to_csv(analysis.home / "detected_change_years.csv")
 
 
 if __name__ == "__main__":
